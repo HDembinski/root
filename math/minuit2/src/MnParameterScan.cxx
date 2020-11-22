7031729db0fa99d32ca2c566805e1a4b8fe235d9
Hans Dembinski <hans.dembinski@gmail.com>
2020-11-22 18:03:57 +0100

    adds debug output and uses limits instead of errors as scanning bounds

--- math/minuit2/src/MnParameterScan.cxx
+++ math/minuit2/src/MnParameterScan.cxx
@@ -9,6 +9,7 @@
 
 #include "Minuit2/MnParameterScan.h"
 #include "Minuit2/FCNBase.h"
+#include "Minuit2/MnPrint.h"
 
 namespace ROOT {
 
@@ -22,35 +23,28 @@ MnParameterScan::MnParameterScan(const FCNBase& fcn, const MnUserParameters& par
 std::vector<std::pair<double, double> > MnParameterScan::operator()(unsigned int par, unsigned int maxsteps, double low, double high) {
    // do the scan for parameter par between low and high values
 
+   MnPrint print("MnParameterScan");
+
    //if(maxsteps > 101) maxsteps = 101;
    std::vector<std::pair<double, double> > result; result.reserve(maxsteps+1);
    std::vector<double> params = fParameters.Params();
-   result.push_back(std::pair<double, double>(params[par], fAmin));
+   result.emplace_back(params[par], fAmin);
 
    if(low > high) return result;
    if(maxsteps < 2) return result;
 
+   const auto& p = fParameters.Parameter(par);
+
    if(low == 0. && high == 0.) {
-      low = params[par] - 2.*fParameters.Error(par);
-      high = params[par] + 2.*fParameters.Error(par);
-   }
-
-   if(low == 0. && high == 0. && fParameters.Parameter(par).HasLimits()) {
-      if(fParameters.Parameter(par).HasLowerLimit())
-         low = fParameters.Parameter(par).LowerLimit();
-      if(fParameters.Parameter(par).HasUpperLimit())
-         high = fParameters.Parameter(par).UpperLimit();
-   }
-
-   if(fParameters.Parameter(par).HasLimits()) {
-      if(fParameters.Parameter(par).HasLowerLimit())
-         low = std::max(low, fParameters.Parameter(par).LowerLimit());
-      if(fParameters.Parameter(par).HasUpperLimit())
-         high = std::min(high, fParameters.Parameter(par).UpperLimit());
+      low = p.HasLowerLimit() ? fParameters.Parameter(par).LowerLimit() : params[par] - 2.*fParameters.Error(par);
+      high = p.HasUpperLimit() ? fParameters.Parameter(par).UpperLimit() : params[par] + 2.*fParameters.Error(par);
    }
 
    double x0 = low;
    double stp = (high - low)/double(maxsteps - 1);
+
+   print.Info("Bounds", low, high, "Nsteps", maxsteps);
+
    for(unsigned int i = 0; i < maxsteps; i++) {
       params[par] = x0 + double(i)*stp;
       double fval = fFCN(params);
@@ -58,8 +52,19 @@ std::vector<std::pair<double, double> > MnParameterScan::operator()(unsigned int
          fParameters.SetValue(par, params[par]);
          fAmin = fval;
       }
-      result.push_back(std::pair<double, double>(params[par], fval));
+      print.Debug([&](std::ostream& os) {
+         for (const auto& p : params)
+           os << p << " ";
+         os << ": " << fval;
+      });
+      result.emplace_back(params[par], fval);
    }
+
+   print.Info([&](std::ostream& os) {
+      os << "New minimum " <<  fAmin << " at parameter values";
+      for (const auto& p : fParameters.Parameters())
+        os << " " << p.Value();
+   });
 
    return result;
 }
